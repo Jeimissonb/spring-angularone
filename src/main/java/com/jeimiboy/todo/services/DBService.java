3c81753b0a6c4de786c8add1768b8fc0b57e8f28
Jeimissonb <jeimissonb@hotmail.com>
2021-05-23 15:48:15 -0300

    Criando um método listOpen para buscar todos os id's (todos) em aberto

--- src/main/java/com/jeimiboy/todo/services/DBService.java
+++ src/main/java/com/jeimiboy/todo/services/DBService.java
@@ -18,11 +18,19 @@ public class DBService {
 
 	public void instanciaBaseDeDados() {
 
-	
-
 		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+
+		
 		Todo t1 = new Todo(null, "Estudar", "Estudar SpringBoot 2 e Angular 11",
 				LocalDateTime.parse("25/03/2022 10:40", formatter), false);
-		todoRepository.saveAll(Arrays.asList(t1));
+		Todo t2 = new Todo(null, "Treinar", "Fisiologia do exercício",
+				LocalDateTime.parse("26/03/2022 11:40", formatter), true);
+		Todo t3 = new Todo(null, "Caminhar", "Exercício cardiovascular eficiente",
+				LocalDateTime.parse("27/03/2022 12:40", formatter), false);
+		Todo t4 = new Todo(null, "Dormir", "Como ter um sono pesado",
+				LocalDateTime.parse("28/03/2022 13:40", formatter), true);
+		
+		
+		todoRepository.saveAll(Arrays.asList(t1, t2, t3, t4));
 	}
 }
--- src/main/java/com/jeimiboy/todo/services/TodoService.java
+++ src/main/java/com/jeimiboy/todo/services/TodoService.java
@@ -1,5 +1,6 @@
 package com.jeimiboy.todo.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,6 +18,12 @@ public class TodoService {
 		public Todo findById (Integer id) {
 			Optional <Todo> obj = repository.findById(id);
 			return obj.orElse(null);
+		}
+
+		public List<Todo> findAllOpen() {
+			List <Todo> list = repository.findAllOpen();
+			return list;
+			
 		}
 	
 }
